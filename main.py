71fe6f5121fe2a0dc217453c51c400c869887f81
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-06 18:37:10 -0300

    Terceiro commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import imaplib
-#import email
-import time
+from email import message_from_bytes
 
 def login(email, senha, conexao):
     try:
@@ -13,20 +12,45 @@ def login(email, senha, conexao):
     except Exception as e:
         print('erro:', e)
 
-def caixa_de_entreda(conexao):
+def exibir_email(conexao):
     conexao.select('inbox')
-    status, mensagem = conexao.search(None, 'BEFORE 01-jul-2024')
+    status, mensagem = conexao.search(None, 'ALL')
 
-    if status == "OK":
-        ids = mensagem[0].split()
-        id = ids[-5:]
-        print(id)
+    ids = mensagem[0].split()
+    id = ids[-5:]
 
-imap_server = "imap.gmail.com"
-conexao = imaplib.IMAP4_SSL(imap_server)
-email = 'jhonatacanevare6@gmail.com'
-senha = 'memmxpgemsxtjhnx'
+    for num in id:
+        status, dados = conexao.fetch(num, '(RFC822)')
+        email_bruto = dados[0][1]
+        msg = message_from_bytes(email_bruto)
+        de = msg.get("From")
+        assunto = msg.get("Subject")
 
-login(email, senha, conexao)
+        print(f'ID: {num}')
+        print(f'De: {de}')
+        print(f'Assunto: {assunto}')
+        
+        print()
 
-caixa_de_entreda(conexao)
+def remover_email(conexao, id_email):
+    conexao.copy(id_email, '[Gmail]/Trash')
+    status, _ = conexao.store(id_email, '+FLAGS', '\\Deleted')
+    if status == 'OK':
+        conexao.expunge()
+        print(f'E-mail {id_email} removido')
+    else:
+        print('Erro ao excluir o email')
+
+def main():
+    imap_server = "imap.gmail.com"
+    conexao = imaplib.IMAP4_SSL(imap_server)
+    usuario = 'jhonatacanevare6@gmail.com'
+    senha = 'memmxpgemsxtjhnx'
+
+    login(usuario, senha, conexao)
+
+    exibir_email(conexao)
+
+    remover_email(conexao, '9120')
+
+main()
